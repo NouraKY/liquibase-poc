adde8c1bafec19f8f2e157995d8e6c6bd2100993
nalyahya <Aa123123>
2020-07-19 12:15:15 +0300

    add jenkins file

---
+++ .groovy
@@ -0,0 +1,14 @@
+
+pipline{
+  agent none
+  stages {
+    stage("Build"){
+      steps {
+        echo 'inside build step'
+        catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
+          sh "mvn liquibase:rollback -Dliquibase.rollbackCount=1"
+        }
+      }
+    }
+  }
+}
