07a769ce11c57b0b1a634dc3ad41ab51d4b480b0
gadahoth <gadah.oth@gmail.com>
2020-10-27 17:04:51 +0300

    project failure

--- src/main/java/com/elm/liquibase/service/liquibase.java
+++ src/main/java/com/elm/liquibase/service/liquibase.java
@@ -4,7 +4,7 @@ public class liquibase{
 
     public static void main(String[] args){
 
-        System.out.println("Testing the app!");
+        System.out.printlnmm("Testing the app!");
 
     }
 }
